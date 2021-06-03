1f8f77ce5a7a39a28952f2bec69be4de6722f609
sathviksaya <sathviksaya@gmail.com>
2021-06-04 01:49:26 +0530

    transactions Screen done

--- task1/bankingapp/lib/TransactionScreen.dart
+++ task1/bankingapp/lib/TransactionScreen.dart
@@ -12,6 +12,7 @@ class TransactionScreen extends StatefulWidget {
 class _TransactionScreenState extends State<TransactionScreen> {
   bool isLoading = true;
   List<Map> list;
+  bool filter = true;
 
   void getTransactions() async {
     var databasesPath = await getDatabasesPath();
@@ -22,9 +23,8 @@ class _TransactionScreenState extends State<TransactionScreen> {
     );
 
     list = await database.rawQuery('''SELECT * FROM Transactions 
-        WHERE sender = \"${widget.currentUser['email']}\" OR 
-        recipient = \"${widget.currentUser['email']}\";''');
-    print(list.toString());
+        WHERE sender = \"${widget.currentUser['username']}\" OR 
+        recipient = \"${widget.currentUser['username']}\";''');
     setState(() {
       isLoading = false;
     });
@@ -48,17 +48,178 @@ class _TransactionScreenState extends State<TransactionScreen> {
           ? Center(
               child: CircularProgressIndicator(),
             )
-          : ListView.builder(
-              itemCount: list.length,
-              itemBuilder: (context, index) {
-                if (list == null) {
-                  return Center(
-                    child: Text("No Transactions..."),
-                  );
-                }
-                return Text(list[index]['sender']);
-              },
-            ),
+          : list == []
+              ? Center(
+                  child: const Text("No Transactions..."),
+                )
+              : Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Filter: ",
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          filter ? "Latest first" : "Latest last",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        Switch(
+                            value: filter,
+                            onChanged: (val) {
+                              setState(() {
+                                filter = val;
+                              });
+                            })
+                      ],
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height - 150,
+                      child: ListView.builder(
+                        physics: BouncingScrollPhysics(),
+                        itemCount: list.length,
+                        itemBuilder: (context, index) {
+                          var transaction = filter
+                              ? list[list.length - index - 1]
+                              : list[index];
+                          return Padding(
+                            padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                            child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              color: widget.currentUser['username'] ==
+                                      transaction['sender']
+                                  ? Colors.red[300]
+                                  : Colors.green[300],
+                              child: ListTile(
+                                title: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Text(
+                                        transaction['sender'],
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Icon(Icons.arrow_forward),
+                                      Text(
+                                        transaction['recipient'],
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                subtitle: Column(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        children: [
+                                          const Text(
+                                            "Amount Transferred:",
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              color: Colors.black54,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            "Rs. " +
+                                                transaction['amount']
+                                                    .toString(),
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              color: Colors.black87,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        children: [
+                                          const Text(
+                                            "Transferred On:",
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              color: Colors.black54,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            transaction['date'].toString(),
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              color: Colors.black87,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: [
+                                          const Text(
+                                            "Current Balance:",
+                                            style: TextStyle(
+                                              fontSize: 15,
+                                              color: Colors.black54,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Text(
+                                            widget.currentUser['username'] ==
+                                                    transaction['sender']
+                                                ? "Rs. ${transaction['senderBalance'].toStringAsFixed(2)}"
+                                                : "Rs. ${transaction['recipientBalance'].toStringAsFixed(2)}",
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              color: Colors.black87,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
     );
   }
 }
--- task1/bankingapp/lib/main.dart
+++ task1/bankingapp/lib/main.dart
@@ -38,7 +38,7 @@ class _InitDBState extends State<InitDB> {
   void initDatabase() async {
     var databasesPath = await getDatabasesPath();
     String path = databasesPath + 'banking.db';
-    // await deleteDatabase(path);
+    await deleteDatabase(path);
     Database database = await openDatabase(
       path,
       version: 1,
@@ -62,16 +62,16 @@ class _InitDBState extends State<InitDB> {
         await db.execute(''' 
           INSERT INTO Users(username, email, mobile, balance) 
           VALUES 
-          ("Max Joseph", "sam12345@gmail.com", 5587946632, 1456.789), 
-          ("Jose Marcel", "marceljose@gmail.com", 8565488932, 15502.32),
-          ("Rose Query", "rose1996@gmail.com", 776933156, 9502.15),
-          ("Walt Greffor", "mwaltgreffor@yahoo.com", 6697553216, 42100.05),
-          ("Britney Villstone", "vilstone123@gmail.com", 7769883215, 12500.5),
-          ("Steve Rogers", "steve@gmail.com", 7860274874, 13446.789), 
-          ("Tony Stark", "tony@gmail.com", 03928475934, 1550345.32),
-          ("Clint Barton", "clint@gmail.com", 39329834134, 95556.15),
-          ("Natasha Romanoff", "nat@yahoo.com", 23094798375, 43400.05),
-          ("Wanda", "wanda@gmail.com", 2136383243, 12500.5);''');
+          ("Bruce Banner", "bruce@gmail.com", 9362836483, 250000), 
+          ("Vision", "vision@gmail.com", 8291447287, 100000),
+          ("TChalla", "tchalla@gmail.com", 8204753843, 800000),
+          ("Peter Parker", "peter@yahoo.com", 997256643, 8000),
+          ("Thor", "thor@gmail.com", 6367682683, 1000),
+          ("Steve Rogers", "steve@gmail.com", 9731601403, 25000), 
+          ("Tony Stark", "tony@gmail.com", 9123834234, 5000000),
+          ("Clint Barton", "clint@gmail.com", 9462327856, 35000),
+          ("Natasha Romanoff", "natasha@yahoo.com", 7291644860, 35000),
+          ("Wanda", "wanda@gmail.com", 7482783966, 40000);''');
       },
     );
     list = await database.rawQuery('SELECT * FROM Users');
