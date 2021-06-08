39dd393d70eb0923affc66dcf4d03f856c6dc79e
sathviksaya <sathviksaya@gmail.com>
2021-06-08 21:36:57 +0530

    google signin done

--- task1/signinapp/lib/homeScreen.dart
+++ task1/signinapp/lib/homeScreen.dart
@@ -1,10 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("SignIn App-Home"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: Text("LogOut",
+                style: TextStyle(
+                  color: Colors.white,
+                )),
+          ),
+        ],
+      ),
       body: Center(
         child: Text("Welcome User"),
       ),
